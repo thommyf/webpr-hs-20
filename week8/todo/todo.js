a8f5966c335988ef5251d88c4cf1919d83ca1a72
Dierk Koenig <dierk.koenig@canoo.com>
2020-11-03 17:01:39 +0100

    towards a todo app with moves: FITYMI, replace constants with variables

--- week8/todo/todo.js
+++ week8/todo/todo.js
@@ -3,6 +3,23 @@ function startTodo() {
 
 }
 
+let todoCounter = 0;
+
 function addTodo() {
+    todoCounter++;
+    const tableRowContent = `        
+        <tr>
+                <td>
+                    <label for="todo_text_${todoCounter}">Todo Text ${todoCounter}</label>
+                    <input id ="todo_text_${todoCounter}" type="text" value="mein erstes todo">
+                </td>
+                <td>
+                    <label for="todo_done_${todoCounter}">Todo Done ${todoCounter}</label>
+                    <input id ="todo_done_${todoCounter}" type="checkbox" checked>
+                </td>
+            </tr>
+`
+    const tableBody = document.getElementById("todoContainer");
+    tableBody.innerHTML += tableRowContent;
     
 }
